475de939a49027e587893d58ce62a37c68f9ddbe
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-03-15 13:54:48 +0100

    el readme

--- src/main/Test/src/test/kotlin/MainKtTest.kt
+++ src/main/Test/src/test/kotlin/MainKtTest.kt
@@ -1,18 +1,19 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
-import org.practicatrim2.normalizar
+
+/**
+ * En el text comprobamos la funcionalidad del .normalizar() para ver si normaliza de forma aducuada y si tiene problema con String de numeros
+ */
 
 class MainKtTest {
 
     @Test
     fun normalizar() {
         val parametrosValidos = "pepe"
-        val parametrosInvalidos = ""
-
-
-
+        val parametroSinEfecto = "122"
 
         assertEquals("Pepe",parametrosValidos.normalizar())
+        assertEquals("122",parametroSinEfecto.normalizar())
     }
 }
